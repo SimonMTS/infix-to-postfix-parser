25eb852067e3896833dcd9e1f6729232b4812f09
Simon Striekwold <info@simonstriekwold.nl>
2022-07-16 07:45:56 +0200

    Revert "broken/incorrect state"
    This reverts commit 0ed795a8c15d5374811ff2a3213a7ed2ef5da278.

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 )
 
 func main() {
-    infix := "345 + 567 * 789 - 2000"
-    // 567 789 * 345 + 2000 -
+    infix := "345 + 567 + 789 - 2000"
 
     tokens := lex(infix)
     // for _, t := range tokens {
@@ -26,7 +25,6 @@ func main() {
 
 // RPN emitter
 func emit(n *Node) (rpn string) {
-    if n == nil { return "" }
     if len(n.children) >= 1 { rpn += emit(n.children[0]) }
     if len(n.children) >= 2 { rpn += emit(n.children[1]) }
     return rpn + string(n.content) + " "
@@ -46,9 +44,7 @@ func parse(tokens []Lexeme) (root Node) {
     matchOperator := func (ctx *Context) Lexeme {
         l := ctx.lookahead()
         if l.equals(Lexeme("+")) ||
-           l.equals(Lexeme("-")) ||
-           l.equals(Lexeme("*")) ||
-           l.equals(Lexeme("/")) {
+           l.equals(Lexeme("-")) {
             ctx.advance()
             return l
         } else {
@@ -66,28 +62,9 @@ func parse(tokens []Lexeme) (root Node) {
     }
     // terminal matchers
 
-    matchMultiplicative := func (ctx *Context, n *Node) (localRoot *Node) {
-        // localRoot := &Node{ make([]*Node, 0), matchNumber(ctx)}
-        localRoot = n
 
-        for ctx.lookahead().equals(Lexeme("*")) ||
-            ctx.lookahead().equals(Lexeme("/")) {
-            op1 := Node{ make([]*Node, 0), matchOperator(ctx)}
-            n2 := Node{ make([]*Node, 0), matchNumber(ctx)}
-
-            op1.children = append(op1.children, localRoot)
-            op1.children = append(op1.children, &n2)
-            localRoot = &op1
-        }
-
-        // active.children = append(active.children, localRoot)
-        return
-    }
-    _ = matchMultiplicative
-
-    matchAdditive := func (ctx *Context, n *Node) (localRoot *Node) {
-        // localRoot := &Node{ make([]*Node, 0), matchNumber(ctx)}
-        localRoot = n
+    matchAdditive := func (ctx *Context) {
+        localRoot := &Node{ make([]*Node, 0), matchNumber(ctx)}
 
         for ctx.lookahead().equals(Lexeme("+")) ||
             ctx.lookahead().equals(Lexeme("-")) {
@@ -99,30 +76,10 @@ func parse(tokens []Lexeme) (root Node) {
             localRoot = &op1
         }
 
-        // active.children = append(active.children, localRoot)
-        return
-    }
-
-    matchExpr := func (ctx *Context) {
-        localRoot := &Node{ make([]*Node, 0), matchNumber(ctx)}
-
-        if ctx.lookahead().equals(Lexeme("+")) ||
-           ctx.lookahead().equals(Lexeme("-")) {
-               localRoot = matchAdditive(ctx, localRoot)
-        }
-        // active.children = append(active.children, localRoot)
-
-        if ctx.lookahead().equals(Lexeme("*")) ||
-           ctx.lookahead().equals(Lexeme("/")) {
-               localRoot = matchMultiplicative(ctx, localRoot)
-        }
-
         active.children = append(active.children, localRoot)
     }
-    _ = matchExpr
 
-    // matchAdditive(&ctx)
-    matchExpr(&ctx)
+    matchAdditive(&ctx)
 
     return
 }
